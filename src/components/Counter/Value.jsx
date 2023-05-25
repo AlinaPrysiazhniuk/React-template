d867b6f2c8e69675e3e974a5d724d7ae20e34d53
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-05-25 12:39:05 +0100

    module-5-1

--- src/components/Counter/Value.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import css from "./Counter.module.css"
-
-const Value = ({onValue}) => (
-    <span className={css.Counter_value}>{onValue}</span>
-)
-
-export default Value;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+//import { useEffect } from 'react';
+
+const Home = () => {
+  return <div>Домашня сторінка :)</div>;
+  // useEffect(()=>{
+  //HTTP - запит
+  //}, [])
+};
+
+export default Home;
